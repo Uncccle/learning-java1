9c4773bf970be9a13ac5cea90981ef856f594570
Uncccle <65302131+Uncccle@users.noreply.github.com>
2020-05-21 12:05:50 +1000

    Add files via upload

---
+++ new1.java
@@ -0,0 +1,13 @@
+public class new1{
+	public static void main (String[] args){
+		int a=1;
+		byte b = 127;
+		short c = 22222;
+		long d = 22222222222222222L;
+		float e = 2.6666666666F;
+		float f = 2.6666666666F;
+		char s= 'a' ;
+		System.out.println(s+1);
+	}
+}
+
